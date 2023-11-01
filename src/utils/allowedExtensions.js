c3e5cc0f7ae7b595c48915b7357c91444a718ea7
Omar-Abosea3 <oa615010@gmail.com>
2023-11-01 13:26:14 +0200

    extension

--- src/utils/allowedExtensions.js
+++ src/utils/allowedExtensions.js
@@ -1,5 +1,5 @@
 const allowedEstensions = {
-    Images:['image/jpg' , 'image/png' , 'image/jpeg' , 'image/gif'],
+    Images:['image/jpg' , 'image/png' , 'image/jpeg' , 'image/gif' , 'image/webp'],
     Videos:['video/mp4']
 }
 export default allowedEstensions;
