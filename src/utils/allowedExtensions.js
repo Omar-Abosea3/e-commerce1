e0e41bcbab703a829a3dfd5eab54b87ee42fd680
Omar-Abosea3 <oa615010@gmail.com>
2024-02-28 19:31:47 +0200

    new edit

--- src/utils/allowedExtensions.js
+++ src/utils/allowedExtensions.js
@@ -1,5 +1,5 @@
 const allowedEstensions = {
-    Images:['image/jpg' , 'image/png' , 'image/jpeg' , 'image/gif' , 'image/webp' , 'image/avif'],
+    Images:['image/jpg' , 'image/png' , 'image/jpeg' , 'image/gif' , 'image/webp' , 'image/avif' , 'image/svg' , 'image/tiff' , 'image/bmp' , 'image/heic' , 'image/heif' ],
     Videos:['video/mp4']
 }
 export default allowedEstensions;
