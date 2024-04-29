9243a5f92e4b56a7327d74243e63877cdbac3fec
Omar-Abosea3 <oa615010@gmail.com>
2024-04-29 05:29:42 +0300

    edits

--- src/modules/Products/product.controller.js
+++ src/modules/Products/product.controller.js
@@ -354,9 +354,8 @@ export const filterProducts = asyncHandeller(async (req, res, next) => {
         select:'name image'
       }
   ]).select('title arTitle desc arDesc slug arSlug colors sizes price priceAfterDiscount brandId rate images categoryId subCategoryId'), req.query)
-      .pagination()
       .sort()
-      .filters()     
+      .filters()
     products = await ApiFeaturesInstance.mongooseQuery;
   }else{
     products = await productModel.find({}).populate([
