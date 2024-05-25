ef60436b611c06d52c5ac8457f066b413c46662a
Omar-Abosea3 <oa615010@gmail.com>
2024-05-25 20:31:47 +0300

    edits

--- src/utils/pagination.js
+++ src/utils/pagination.js
@@ -1,11 +1,7 @@
-const paginationFunction = ({page = 1 , size = 1}) => {
-    if(page < 1) page = 1;
-    if(size < 1) size = 1;
-
-    const limit = size;
-    const skip = (page - 1) * size ;
-
-    return { limit , skip};
+const paginationFunction = ({page = 1 , size = 10}) => {
+    const limit = Math.max(size, 1); // ensure size is at least 1
+    const skip = (Math.max(page, 1) - 1) * limit; // ensure page is at least 1
+    return { limit, skip };
 }
 
 export default paginationFunction;
